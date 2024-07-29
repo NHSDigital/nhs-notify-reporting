dc8062a9a82595b45f874833075e5bbc6961cc13
Mark Pullan <mark.pullan@bjss.com>
2024-07-29 12:11:12 +0100

    Rename source data catalogue

--- infrastructure/terraform/components/reporting/athena_named_query_reporting.tf
+++ infrastructure/terraform/components/reporting/athena_named_query_reporting.tf
@@ -2,6 +2,6 @@ resource "aws_athena_named_query" "reporting" {
   name        = "${local.csi}-reporting"
   description = "Simple named query for ${local.csi}"
   workgroup   = aws_athena_workgroup.reporting.id
-  database    = "${aws_athena_data_catalog.reporting.name}.comms-${local.parameter_bundle.environment}-api-rpt-reporting"
-  query       = "SELECT * FROM \"${aws_athena_data_catalog.reporting.name}\".\"comms-${var.core_env}-api-rpt-reporting\".\"transaction_history\" limit 10"
+  database    = "${aws_athena_data_catalog.source_data.name}.comms-${local.parameter_bundle.environment}-api-rpt-reporting"
+  query       = "SELECT * FROM \"${aws_athena_data_catalog.source_data.name}\".\"comms-${var.core_env}-api-rpt-reporting\".\"transaction_history\" limit 10"
 }
