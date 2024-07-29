1615582026c645ef1b9054cd8ca0d14a91f89356
Mark Pullan <mark.pullan@bjss.com>
2024-07-29 14:13:12 +0100

    Script name change

--- infrastructure/terraform/components/reporting/null_resource_create_table.tf
+++ infrastructure/terraform/components/reporting/null_resource_create_table.tf
@@ -3,7 +3,7 @@ resource "null_resource" "create_table" {
     always_run = timestamp()
   }
   provisioner "local-exec" {
-    command = "${path.module}/scripts/create_iceberg_table.sh ${var.environment} ${local.this_account}"
+    command = "${path.module}/scripts/create_table.sh ${var.environment} ${local.this_account}"
   }
 
   depends_on = [aws_athena_workgroup.setup, aws_athena_workgroup.ingestion]
