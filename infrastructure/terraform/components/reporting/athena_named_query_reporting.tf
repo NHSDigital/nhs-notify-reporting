0b1e5f3863deaa1e20a99a055fa18f05244a95d8
Mark Pullan <mark.pullan@bjss.com>
2024-07-29 16:25:26 +0100

    Change ingestion ownership

--- infrastructure/terraform/components/reporting/athena_named_query_reporting.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_athena_named_query" "reporting" {
-  name        = "${local.csi}-reporting"
-  description = "Simple named query for ${local.csi}"
-  workgroup   = aws_athena_workgroup.ingestion.id
-  database    = "${aws_athena_data_catalog.source_data.name}.comms-${local.parameter_bundle.environment}-api-rpt-reporting"
-  query       = "SELECT * FROM \"${aws_athena_data_catalog.source_data.name}\".\"comms-${var.core_env}-api-rpt-reporting\".\"transaction_history\" limit 10"
-
-  depends_on = [null_resource.request_item_plan_summary_table]
-}
