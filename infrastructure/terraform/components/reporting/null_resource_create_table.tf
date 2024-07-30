c0aceb93f1732450302b9b00f48589393a6fb5b1
Mark Pullan <mark.pullan@bjss.com>
2024-07-30 17:41:55 +0100

    Fix file renames

---
+++ infrastructure/terraform/components/reporting/null_resource_create_table.tf
@@ -0,0 +1,10 @@
+resource "null_resource" "create_table" {
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/create_iceberg_table.sh ${var.environment} ${local.this_account}"
+  }
+
+  depends_on = [aws_athena_workgroup.reporting]
+}
