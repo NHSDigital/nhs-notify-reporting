66ce52a92de9f01f2660cf5d7fe378e494bbd7ce
Mark Pullan <mark.pullan@bjss.com>
2024-07-31 18:10:00 +0100

    Use secret and SHA256 in temp query

--- infrastructure/terraform/components/reporting/scripts/sql/queries/temp_query.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/queries/temp_query.sql
@@ -1,1 +1,6 @@
-SELECT * FROM ${source_table} limit 10;
+SELECT
+  requestitemid,
+  TO_BASE64(SHA256(CAST('env_secret' || '.' || requestitemid AS varbinary))) as requestitemhash
+FROM ${source_table}
+WHERE (sk LIKE 'REQUEST_ITEM#%')
+LIMIT 10;
