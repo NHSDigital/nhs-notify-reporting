b512a4c437049a13c235a9f6b3dd5fc3736cbe93
Mark Pullan <mark.pullan@bjss.com>
2024-08-02 15:33:12 +0100

    Fix broken parameter substitution

--- infrastructure/terraform/components/reporting/scripts/sql/queries/temp_query.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/queries/temp_query.sql
@@ -1,9 +1,6 @@
-/*
 SELECT
   requestitemid,
   TO_BASE64(SHA256(CAST(? || '.' || requestitemid AS varbinary))) as requestitemhash
 FROM ${source_table}
 WHERE (sk LIKE 'REQUEST_ITEM#%')
-LIMIT 10;
-*/
-SELECT ? FROM ${source_table} LIMIT 1; 
+LIMIT 1;
