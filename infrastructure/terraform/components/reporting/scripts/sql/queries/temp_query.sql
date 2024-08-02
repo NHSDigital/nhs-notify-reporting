0464fa0045e53d0cebb45ff99d0a4702dab9ba60
Mark Pullan <mark.pullan@bjss.com>
2024-08-02 15:20:58 +0100

    Hash test

--- infrastructure/terraform/components/reporting/scripts/sql/queries/temp_query.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/queries/temp_query.sql
@@ -1,6 +1,9 @@
+/*
 SELECT
   requestitemid,
   TO_BASE64(SHA256(CAST(? || '.' || requestitemid AS varbinary))) as requestitemhash
 FROM ${source_table}
 WHERE (sk LIKE 'REQUEST_ITEM#%')
 LIMIT 10;
+*/
+SELECT $ FROM ${source_table} LIMIT 1; 
