d547f7097b38accdf21e5437969bd27c504061cb
Mark Pullan <mark.pullan@bjss.com>
2024-08-02 10:19:47 +0100

    Parameterise key

--- infrastructure/terraform/components/reporting/scripts/sql/queries/temp_query.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/queries/temp_query.sql
@@ -1,6 +1,6 @@
 SELECT
   requestitemid,
-  TO_BASE64(SHA256(CAST('env_secret' || '.' || requestitemid AS varbinary))) as requestitemhash
+  TO_BASE64(SHA256(CAST(? || '.' || requestitemid AS varbinary))) as requestitemhash
 FROM ${source_table}
 WHERE (sk LIKE 'REQUEST_ITEM#%')
 LIMIT 10;
