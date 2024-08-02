ac1fc0c2b7b3a879e1b9f7f589133e77ceb5e54f
Mark Pullan <mark.pullan@bjss.com>
2024-08-02 15:51:09 +0100

    TF naming convention fix

--- infrastructure/terraform/components/reporting/aws_ssm_parameter_hash_key.tf
+++ infrastructure/terraform/components/reporting/aws_ssm_parameter_hash_key.tf
@@ -1,10 +1,10 @@
-resource "random_bytes" "hash_key" {
-  length = 32
-}
-
 resource "aws_ssm_parameter" "hash_key" {
   name        = "/${local.csi}/hash_key"
   description = "Random key used to generate distinct environment-specific hash values"
   type        = "SecureString"
   value       = random_bytes.hash_key.base64
+}
+
+resource "random_bytes" "hash_key" {
+  length = 32
 }
