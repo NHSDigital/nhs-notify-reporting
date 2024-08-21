c265639c8919dcbd2077d1e4c6d57a5eaf9a625c
Mark Pullan <mark.pullan@bjss.com>
2024-08-21 09:17:07 +0100

    Policies for new buckets

--- infrastructure/terraform/components/reporting/s3_bucket_policy_data.tf
+++ infrastructure/terraform/components/reporting/s3_bucket_policy_data.tf
@@ -1,15 +1,15 @@
-resource "aws_s3_bucket_policy" "reporting" {
-  bucket = aws_s3_bucket.reporting.id
-  policy = data.aws_iam_policy_document.reporting.json
+resource "aws_s3_bucket_policy" "data" {
+  bucket = aws_s3_bucket.data.id
+  policy = data.aws_iam_policy_document.data.json
 }
 
-data "aws_iam_policy_document" "reporting" {
+data "aws_iam_policy_document" "data" {
   statement {
     effect  = "Deny"
     actions = ["s3:*"]
     resources = [
-      aws_s3_bucket.reporting.arn,
-      "${aws_s3_bucket.reporting.arn}/*",
+      aws_s3_bucket.data.arn,
+      "${aws_s3_bucket.data.arn}/*",
     ]
 
     principals {
