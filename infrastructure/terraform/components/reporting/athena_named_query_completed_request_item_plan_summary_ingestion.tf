ef80b2b97eb631651187f3f0e3bfe89e7d20fb28
Mark Pullan <mark.pullan@bjss.com>
2024-08-23 11:14:20 +0100

    Renamed database table to be compliant with naming standards

--- infrastructure/terraform/components/reporting/athena_named_query_completed_request_item_plan_summary_ingestion.tf
+++ infrastructure/terraform/components/reporting/athena_named_query_completed_request_item_plan_summary_ingestion.tf
@@ -1,11 +1,11 @@
-resource "aws_athena_named_query" "completed_request_item_plan_summary_ingestion" {
-  name        = "completed_request_item_plan_summary_ingestion"
-  description = "Updates completed_request_item_plan_summary table based upon a moving time window"
+resource "aws_athena_named_query" "request_item_plan_completed_summary_ingestion" {
+  name        = "request_item_plan_completed_summary_ingestion"
+  description = "Updates request_item_plan_completed_summary table based upon a moving time window"
   workgroup   = aws_athena_workgroup.ingestion.id
   database    = aws_glue_catalog_database.reporting.name
-  query       = templatefile("${path.module}/scripts/sql/queries/completed_request_item_plan_summary.sql", {
+  query       = templatefile("${path.module}/scripts/sql/queries/request_item_plan_completed_summary.sql", {
     source_table= "\"${aws_athena_data_catalog.source_data.name}\".\"comms-${var.core_env}-api-rpt-reporting\".\"transaction_history\""
   })
 
-  depends_on = [null_resource.completed_request_item_plan_summary_table]
+  depends_on = [null_resource.request_item_plan_completed_summary_table]
 }
