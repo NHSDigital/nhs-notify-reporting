839916eb0c05ac13712e3b8b559c214e59185e75
Mark Pullan <mark.pullan@bjss.com>
2024-09-04 12:17:04 +0100

    Add new column for reporting

--- infrastructure/terraform/components/reporting/null_resource_request_item_status_table.tf
+++ infrastructure/terraform/components/reporting/null_resource_request_item_status_table.tf
@@ -14,3 +14,19 @@ resource "null_resource" "request_item_status_table" {
 
   depends_on = [aws_athena_workgroup.setup]
 }
+
+resource "null_resource" "patientodscode_column" {
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+      ${path.module}/scripts/add_column.sh
+        ${aws_athena_workgroup.setup.name} \
+        ${aws_glue_catalog_database.reporting.name} \
+        request_item_status patientodscode string
+    EOT
+  }
+
+  depends_on = [null_resource.request_item_status_table]
+}
