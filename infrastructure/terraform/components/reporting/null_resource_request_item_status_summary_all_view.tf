f4f82fb571de76caa6cae4ec9cb95c79a47b81ad
markpullan <mark.pullan@bjss.com>
2024-10-10 10:38:48 +0100

    Feature/ccm 6986 request items summary view (#66)
    * Add missing null resource
    
    * Update file

---
+++ infrastructure/terraform/components/reporting/null_resource_request_item_status_summary_all_view.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "request_item_status_summary_all_view" {
+  triggers = {
+    sql = filesha256("${path.module}/scripts/sql/views/request_item_status_summary_all.sql")
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+      ${path.module}/scripts/create_replace_view.sh \
+        ${aws_athena_workgroup.setup.name} \
+        ${aws_glue_catalog_database.reporting.name} \
+        request_item_status_summary_all
+    EOT
+  }
+
+  depends_on = [aws_athena_workgroup.setup]
+}
