7c50c79b141c7088f5f104a67edb8cb683931372
Mark Pullan <mark.pullan@bjss.com>
2024-10-14 16:20:03 +0100

    Commit manual changes

--- infrastructure/terraform/components/reporting/ssm_parameter_bob_client_ids.tf
+++ infrastructure/terraform/components/reporting/ssm_parameter_bob_client_ids.tf
@@ -3,5 +3,9 @@ resource "aws_ssm_parameter" "bob_client_ids" {
   description = "List of client ids to for which to generate bob report"
   type        = "String"
   value       = "[]"
+
+  lifecycle {
+    ignore_changes = ["value"]
+  }
 }
 
