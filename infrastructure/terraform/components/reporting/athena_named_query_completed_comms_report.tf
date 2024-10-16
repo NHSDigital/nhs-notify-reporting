8d774000ac3c8de2b8ab17843ff638a8c81ef8b1
Mark Pullan <mark.pullan@bjss.com>
2024-10-16 12:59:12 +0100

    Use correct report name

--- infrastructure/terraform/components/reporting/athena_named_query_completed_comms_report.tf
+++ infrastructure/terraform/components/reporting/athena_named_query_completed_comms_report.tf
@@ -3,7 +3,7 @@ resource "aws_athena_named_query" "completed_comms_report" {
   description = "Runs the qery to generate the completed communications report, writing the results back to the core account"
   workgroup   = aws_athena_workgroup.core.id
   database    = aws_glue_catalog_database.reporting.name
-  query       = file("${path.module}/scripts/sql/egress/completed_comms_report.sql")
+  query       = file("${path.module}/scripts/sql/reports/completed_comms_report.sql")
 
   depends_on = [
     null_resource.request_item_status_table,
