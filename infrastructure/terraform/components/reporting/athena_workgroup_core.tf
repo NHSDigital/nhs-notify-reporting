4da65fa1709ef33cf7136be9542c5f2e2fe0399f
Mark Pullan <mark.pullan@bjss.com>
2024-10-16 17:28:53 +0100

    Use default key

--- infrastructure/terraform/components/reporting/athena_workgroup_core.tf
+++ infrastructure/terraform/components/reporting/athena_workgroup_core.tf
@@ -12,7 +12,7 @@ resource "aws_athena_workgroup" "core" {
 
       encryption_configuration {
         encryption_option = "SSE_KMS"
-        kms_key_arn       = "arn:aws:kms:eu-west-2:${var.core_account_id}:alias/comms-${var.core_env}-api-s3"
+        # kms_key_arn       = "arn:aws:kms:eu-west-2:${var.core_account_id}:alias/comms-${var.core_env}-api-s3"
       }
     }
   }
