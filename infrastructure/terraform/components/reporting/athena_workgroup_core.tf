e0a6de1b1cb3edba3b2346c00b9035f83796fe3b
Mark Pullan <mark.pullan@bjss.com>
2024-10-16 17:31:36 +0100

    Incrementally set bucket info

--- infrastructure/terraform/components/reporting/athena_workgroup_core.tf
+++ infrastructure/terraform/components/reporting/athena_workgroup_core.tf
@@ -7,13 +7,13 @@ resource "aws_athena_workgroup" "core" {
     enforce_workgroup_configuration = false
 
     result_configuration {
-      expected_bucket_owner = var.core_account_id
+      # expected_bucket_owner = var.core_account_id
       output_location       = "s3://comms-${var.core_account_id}-eu-west-2-${var.core_env}-api-rpt-ingress"
 
-      encryption_configuration {
-        encryption_option = "SSE_KMS"
+      #encryption_configuration {
+      #  encryption_option = "SSE_KMS"
         # kms_key_arn       = "arn:aws:kms:eu-west-2:${var.core_account_id}:alias/comms-${var.core_env}-api-s3"
-      }
+      #}
     }
   }
 }
