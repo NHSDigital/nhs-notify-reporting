9c96511a875ce59a70ead13fdd0e8c9a476b4fb7
Mark Pullan <mark.pullan@bjss.com>
2024-10-17 10:47:01 +0100

    Put ACL perms

--- infrastructure/terraform/components/reporting/sfn_state_machine_completed_comms_report.tf
+++ infrastructure/terraform/components/reporting/sfn_state_machine_completed_comms_report.tf
@@ -128,7 +128,8 @@ data "aws_iam_policy_document" "sfn_completed_comms_report" {
       "s3:GetBucketLocation",
       "s3:GetObject",
       "s3:ListBucket",
-      "s3:PutObject"
+      "s3:PutObject",
+      "s3:PutObjectACL"
     ]
 
     resources = [
