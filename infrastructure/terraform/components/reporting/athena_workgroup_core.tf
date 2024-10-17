2353e9df1bdeaf99ab29282c36f69d91fe6195d7
Mark Pullan <mark.pullan@bjss.com>
2024-10-17 09:06:33 +0100

    Use bucket owner

--- infrastructure/terraform/components/reporting/athena_workgroup_core.tf
+++ infrastructure/terraform/components/reporting/athena_workgroup_core.tf
@@ -7,7 +7,7 @@ resource "aws_athena_workgroup" "core" {
     enforce_workgroup_configuration = false
 
     result_configuration {
-      # expected_bucket_owner = var.core_account_id
+      expected_bucket_owner = var.core_account_id
       output_location       = "s3://comms-${var.core_account_id}-eu-west-2-${var.core_env}-api-rpt-ingress"
 
       #encryption_configuration {
