226cc8fd373d686ff90ad981c329237fb31370c0
Mark Pullan <mark.pullan@bjss.com>
2024-10-17 09:10:48 +0100

    Add KMS back in

--- infrastructure/terraform/components/reporting/athena_workgroup_core.tf
+++ infrastructure/terraform/components/reporting/athena_workgroup_core.tf
@@ -10,10 +10,10 @@ resource "aws_athena_workgroup" "core" {
       expected_bucket_owner = var.core_account_id
       output_location       = "s3://comms-${var.core_account_id}-eu-west-2-${var.core_env}-api-rpt-ingress"
 
-      #encryption_configuration {
-      #  encryption_option = "SSE_KMS"
-        # kms_key_arn       = "arn:aws:kms:eu-west-2:${var.core_account_id}:alias/comms-${var.core_env}-api-s3"
-      #}
+      encryption_configuration {
+        encryption_option = "SSE_KMS"
+        kms_key_arn       = "arn:aws:kms:eu-west-2:${var.core_account_id}:alias/comms-${var.core_env}-api-s3"
+      }
     }
   }
 }
