4823e392677b2e5e28194b904b371134b6116abf
Mark Pullan <mark.pullan@bjss.com>
2024-10-17 15:08:51 +0100

    Typo fix

--- infrastructure/terraform/components/reporting/athena_named_query_completed_comms_report.tf
+++ infrastructure/terraform/components/reporting/athena_named_query_completed_comms_report.tf
@@ -1,6 +1,6 @@
 resource "aws_athena_named_query" "completed_comms_report" {
   name        = "completed_comms_report"
-  description = "Runs the qery to generate the completed communications report, writing the results back to the core account"
+  description = "Runs the query to generate the completed communications report, writing the results back to the core account"
   workgroup   = aws_athena_workgroup.core.id
   database    = aws_glue_catalog_database.reporting.name
   query       = file("${path.module}/scripts/sql/reports/completed_comms_report.sql")
