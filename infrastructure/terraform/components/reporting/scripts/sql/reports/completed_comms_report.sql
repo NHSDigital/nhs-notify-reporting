23068cdd23e90134aa36939a4e618068d0d26141
Fawad Ali <fawad.ali3@nhs.net>
2024-11-04 09:08:12 +0000

    CCM-5656: update report for parallel send

--- infrastructure/terraform/components/reporting/scripts/sql/reports/completed_comms_report.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/reports/completed_comms_report.sql
@@ -1,5 +1,6 @@
 SELECT
     ri.requestitemid AS requestitemid,
+    ri.clientid AS clientid,
     DATE(ri.createdtime) AS requestitemcreateddate,
     ri.status AS requestitemstatus,
     rip.requestitemplanid AS requestitemplanid,
@@ -14,8 +15,10 @@ LEFT OUTER JOIN request_item_plan_status rip ON
 WHERE
     ri.clientid = ? AND
     ri.status IN ('FAILED', 'DELIVERED') AND
-    rip.status IN ('FAILED', 'DELIVERED', 'SKIPPED') AND
+    (
+        rip.status is null OR
+        rip.status IN ('FAILED', 'DELIVERED', 'SKIPPED')
+    ) AND
     (
         DATE(ri.completedtime) = DATE(?) OR DATE(rip.completedtime) = DATE(?)
     )
-
