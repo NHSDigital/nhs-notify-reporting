30f248fa4c9cad22edb54bd228a0e6153e78f476
Fawad Ali <fawad.ali3@nhs.net>
2024-11-07 13:21:53 +0000

    CCM-5656: update select to include additional fields

--- infrastructure/terraform/components/reporting/scripts/sql/reports/completed_comms_report.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/reports/completed_comms_report.sql
@@ -1,13 +1,19 @@
 SELECT
+    ri.requestId as requestid,
+    ri.requestRefId as requestrefid,
     ri.requestitemid AS requestitemid,
-    ri.clientid AS clientid,
-    DATE(ri.createdtime) AS requestitemcreateddate,
+    ri.requestItemRefId as requestitemrefid,
+    DATE(ri.completedtime) AS requestitemcompleteddate,
+    ri.failedReason as requestitemfailedreason,
+    ri.sendingGroupId as sendinggroupid,
+    ri.sendingGroupIdVersion as sendinggroupidversion,
     ri.status AS requestitemstatus,
     rip.requestitemplanid AS requestitemplanid,
     DATE(rip.completedtime) AS requestitemplancompleteddate,
     rip.status AS requestitemplanstatus,
     rip.communicationtype AS communicationtype,
     rip.channeltype AS channeltype
+    rip.failedReason as requestitemplanfailedreason
 FROM request_item_status ri
 LEFT OUTER JOIN request_item_plan_status rip ON
     ri.requestitemid = rip.requestitemid AND
