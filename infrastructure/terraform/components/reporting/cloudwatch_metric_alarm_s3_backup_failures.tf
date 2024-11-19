e5472d329310ab569d2b055f87684fa10e62d926
jamesthompson26-nhs <james.thompson26@nhs.net>
2024-11-19 12:06:20 +0000

    CCM-6919: S3 Backups

--- infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_s3_backup_failures.tf
+++ infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_s3_backup_failures.tf
@@ -1,4 +1,6 @@
 resource "aws_cloudwatch_metric_alarm" "s3_backup_failures" {
+  count = var.enable_s3_backup ? 1 : 0
+
   alarm_name                = "${local.csi}-s3-backup-failures"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 1
@@ -11,6 +13,6 @@ resource "aws_cloudwatch_metric_alarm" "s3_backup_failures" {
   insufficient_data_actions = []
 
   dimensions = {
-    BackupVaultName = aws_backup_vault.s3_backup.name
+    BackupVaultName = aws_backup_vault.s3_backup[0].name
   }
 }
