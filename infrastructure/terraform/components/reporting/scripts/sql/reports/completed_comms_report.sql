a50021e3617e1b66425abf160e2e8393eedb7d5e
Fawad Ali <alibombaye@users.noreply.github.com>
2024-11-20 12:53:43 +0000

    CCM-5656: update report for parallel send (#77)
    * CCM-5656: update report for parallel send
    
    * CCM-5656: update sql
    
    * CCM-5656: update sql to check requestitemplanid is null instead of status
    
    * CCM-5656: update select to include additional fields
    
    * CCM-5656: update cron schedule
    
    * CCM-5656: add missing comma
    
    * CCM-5656: exclude skipped
    
    ---------
    
    Co-authored-by: Alex Nuttall <alex.nuttall1@nhs.net>

--- infrastructure/terraform/components/reporting/scripts/sql/reports/completed_comms_report.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/reports/completed_comms_report.sql
@@ -1,21 +1,26 @@
 SELECT
+    ri.requestId as requestid,
+    ri.requestRefId as requestrefid,
     ri.requestitemid AS requestitemid,
-    DATE(ri.createdtime) AS requestitemcreateddate,
+    ri.requestItemRefId as requestitemrefid,
+    DATE(ri.completedtime) AS requestitemcompleteddate,
+    ri.failedReason as requestitemfailedreason,
+    ri.sendingGroupId as sendinggroupid,
+    ri.sendingGroupIdVersion as sendinggroupidversion,
     ri.status AS requestitemstatus,
     rip.requestitemplanid AS requestitemplanid,
     DATE(rip.completedtime) AS requestitemplancompleteddate,
     rip.status AS requestitemplanstatus,
     rip.communicationtype AS communicationtype,
-    rip.channeltype AS channeltype
+    rip.channeltype AS channeltype,
+    rip.failedReason as requestitemplanfailedreason
 FROM request_item_status ri
 LEFT OUTER JOIN request_item_plan_status rip ON
     ri.requestitemid = rip.requestitemid AND
     ri.clientid = rip.clientid
 WHERE
     ri.clientid = ? AND
-    ri.status IN ('FAILED', 'DELIVERED') AND
-    rip.status IN ('FAILED', 'DELIVERED', 'SKIPPED') AND
     (
-        DATE(ri.completedtime) = DATE(?) OR DATE(rip.completedtime) = DATE(?)
+        (rip.status IN ('FAILED', 'DELIVERED') AND DATE(rip.completedtime) = DATE(?)) OR
+        (ri.status IN ('FAILED') AND rip.requestitemplanid is null AND DATE(ri.completedtime) = DATE(?))
     )
-
