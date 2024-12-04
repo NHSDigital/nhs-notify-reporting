cde1bfc83bee0dcb05066dbc129bf3737cfbf0a1
Mark Pullan <mark.pullan@bjss.com>
2024-12-04 12:00:35 +0000

    Fix report sql

--- infrastructure/terraform/components/reporting/scripts/sql/reports/completed_batch_report.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/reports/completed_batch_report.sql
@@ -21,7 +21,7 @@ LEFT OUTER JOIN request_item_plan_status rip ON
     ri.requestitemid = rip.requestitemid AND
     ri.clientid = rip.clientid
 WHERE
-    ri.clientid = ? AND ri.requestid = ?
+    ri.clientid = ? AND ri.requestid = ? AND
     (
         (rip.status IN ('FAILED', 'DELIVERED') AND DATE(rip.completedtime) = DATE(?)) OR
         (ri.status IN ('FAILED') AND rip.requestitemplanid is null AND DATE(ri.completedtime) = DATE(?))
