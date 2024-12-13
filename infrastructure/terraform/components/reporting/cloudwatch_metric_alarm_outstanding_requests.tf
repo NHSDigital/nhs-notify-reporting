f304c5d7be530b14fc540d5f8cf7b2d6bdefdced
Mark Pullan <mark.pullan@bjss.com>
2024-12-13 14:19:59 +0000

    Fire every 3 hours

--- infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_outstanding_requests.tf
+++ infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_outstanding_requests.tf
@@ -2,10 +2,6 @@ resource "aws_cloudwatch_metric_alarm" "outstanding_requests" {
   alarm_name                = "${local.csi}-outstanding-requests"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 1
-  //metric_name               = "OutstandingRequestCount"
-  //namespace                 = "Notify/Watchdog"
-  //period                    = 86400
-  //statistic                 = "Minimum"
   threshold                 = 1
   alarm_description         = "This metric monitors unexpected outstanding requests"
 
@@ -13,6 +9,6 @@ resource "aws_cloudwatch_metric_alarm" "outstanding_requests" {
     id          = "SumOutstandingRequestCount"
     expression  = "SELECT SUM(OutstandingRequestCount) FROM \"Notify/Watchdog\""
     return_data = "true"
-    period      = 86400
+    period      = 10800
   }
 }
