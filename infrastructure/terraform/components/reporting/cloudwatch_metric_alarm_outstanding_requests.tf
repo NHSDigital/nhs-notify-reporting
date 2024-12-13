fbc923d92e9c461f34e85bf6a9b7b2f71f49944a
Mark Pullan <mark.pullan@bjss.com>
2024-12-13 13:46:55 +0000

    Spike on metrics query

--- infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_outstanding_requests.tf
+++ infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_outstanding_requests.tf
@@ -2,10 +2,22 @@ resource "aws_cloudwatch_metric_alarm" "outstanding_requests" {
   alarm_name                = "${local.csi}-outstanding-requests"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 1
-  metric_name               = "OutstandingRequestCount"
-  namespace                 = "Notify/Watchdog"
-  period                    = 86400
-  statistic                 = "Minimum"
+  //metric_name               = "OutstandingRequestCount"
+  //namespace                 = "Notify/Watchdog"
+  //period                    = 86400
+  //statistic                 = "Minimum"
   threshold                 = 1
   alarm_description         = "This metric monitors unexpected outstanding requests"
+
+  metric_query {
+    id = "m1"
+
+    metric {
+      metric_name = "OutstandingRequestCount"
+      namespace   = "Notify/Watchdog"
+      period      = 86400
+      stat        = "Sum"
+      unit        = "Count"
+    }
+  }
 }
