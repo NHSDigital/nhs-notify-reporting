be0026ee5302e5b4d10cb3e74681f46a1128956d
Mark Pullan <mark.pullan@bjss.com>
2024-12-16 09:12:44 +0000

    Detect failed plans earlier based on channel

--- infrastructure/terraform/components/reporting/scripts/sql/watchdog/overdue_request_item_plans.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/watchdog/overdue_request_item_plans.sql
@@ -3,6 +3,13 @@ SELECT
   COUNT(*) - SUM(CASE WHEN status IN ('FAILED', 'DELIVERED', 'SKIPPED') THEN 1 ELSE 0 END)
 FROM request_item_plan_status
 WHERE
-createdtime < DATE_ADD('week', -2, CURRENT_DATE) AND
+(
+    (ordernumber = 1 AND createdtime < DATE_ADD('day', -2, CURRENT_DATE) AND communicationtype = 'NHSAPP') OR
+    (ordernumber = 1 AND createdtime < DATE_ADD('day', -4, CURRENT_DATE) AND communicationtype = 'SMS') OR
+    (ordernumber = 1 AND createdtime < DATE_ADD('day', -4, CURRENT_DATE) AND communicationtype = 'EMAIL') OR
+    (ordernumber = 1 AND createdtime < DATE_ADD('day', -7, CURRENT_DATE) AND communicationtype = 'LETTER') OR
+    (ordernumber = 2 AND createdtime < DATE_ADD('day', -10, CURRENT_DATE)) OR
+    createdtime < DATE_ADD('week', -2, CURRENT_DATE)
+) AND
 createdtime >= DATE_ADD('day', -90, CURRENT_DATE)
 GROUP BY clientid
