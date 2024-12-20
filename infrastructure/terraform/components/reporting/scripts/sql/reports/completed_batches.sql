2f37c571598c6d9f2fc12a2cbc5adf7f18146cbb
Mark Pullan <mark.pullan@bjss.com>
2024-12-20 12:37:44 +0000

    Change query to return requestrefid

--- infrastructure/terraform/components/reporting/scripts/sql/reports/completed_batches.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/reports/completed_batches.sql
@@ -1,11 +1,12 @@
-SELECT requestid, completedtime FROM (
+SELECT requestid, requestrefid FROM (
   SELECT
     requestid,
+    requestrefid,
     COUNT(*) AS totalitems,
     SUM(CASE WHEN status IN ('FAILED', 'DELIVERED') THEN 1 ELSE 0 END) AS completeditems,
     MAX(completedtime) AS completedtime
   FROM request_item_status
   WHERE clientid = ?
-  GROUP BY requestid
+  GROUP BY requestid, requestrefid
 )
 WHERE totalitems=completeditems AND DATE(completedtime) >= DATE(DATE_ADD('week', -1, CURRENT_DATE))
