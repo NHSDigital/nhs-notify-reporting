3e5382b2bf616ce2a3e3829be583e525298a4dec
Mark Pullan <mark.pullan@bjss.com>
2025-01-06 14:24:58 +0000

    Fix capitalisation

--- infrastructure/terraform/components/reporting/scripts/sql/views/completed_comms.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/views/completed_comms.sql
@@ -1,23 +1,23 @@
 CREATE OR REPLACE VIEW ${view_name} AS
 SELECT
     ri.clientid as clientid,
-    ri.requestId as requestid,
-    ri.requestRefId as requestrefid,
+    ri.requestid as requestid,
+    ri.requestrefid as requestrefid,
     ri.requestitemid AS requestitemid,
-    ri.requestItemRefId as requestitemrefid,
+    ri.requestitemrefid as requestitemrefid,
     DATE(ri.completedtime) AS requestitemcompleteddate,
-    ri.failedReason as requestitemfailedreason,
-    ri.sendingGroupId as sendinggroupid,
-    ri.sendingGroupIdVersion as sendinggroupidversion,
-    ri.sendingGroupName as sendinggroupname,
-    ri.sendingGroupCreatedDate as sendinggroupcreateddate,
+    ri.failedreason as requestitemfailedreason,
+    ri.sendinggroupid as sendinggroupid,
+    ri.sendinggroupidversion as sendinggroupidversion,
+    ri.sendinggroupname as sendinggroupname,
+    ri.sendinggroupcreateddate as sendinggroupcreateddate,
     ri.status AS requestitemstatus,
     rip.requestitemplanid AS requestitemplanid,
     DATE(rip.completedtime) AS requestitemplancompleteddate,
     rip.status AS requestitemplanstatus,
     rip.communicationtype AS communicationtype,
     rip.channeltype AS channeltype,
-    rip.failedReason as requestitemplanfailedreason
+    rip.failedreason as requestitemplanfailedreason
 FROM request_item_status ri
 LEFT OUTER JOIN request_item_plan_status rip ON
     ri.requestitemid = rip.requestitemid AND
