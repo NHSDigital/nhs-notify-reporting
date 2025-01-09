9890cb38e80db3852c53d5252ab3ff7e1ce242bf
Mark Pullan <mark.pullan@bjss.com>
2025-01-09 12:01:44 +0000

    Add campaignid as dimension

--- infrastructure/terraform/components/reporting/scripts/sql/watchdog/overdue_request_item_plans.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/watchdog/overdue_request_item_plans.sql
@@ -1,5 +1,6 @@
 SELECT
   clientid,
+  campaignid,
   COUNT(*) - SUM(CASE WHEN status IN ('FAILED', 'DELIVERED', 'SKIPPED') THEN 1 ELSE 0 END)
 FROM request_item_plan_status
 WHERE
@@ -12,4 +13,4 @@ WHERE
     createdtime < DATE_ADD('week', -2, CURRENT_DATE)
 ) AND
 createdtime >= DATE_ADD('day', -90, CURRENT_DATE)
-GROUP BY clientid
+GROUP BY clientid, campaignid
--- infrastructure/terraform/components/reporting/scripts/sql/watchdog/overdue_requests.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/watchdog/overdue_requests.sql
@@ -1,13 +1,13 @@
-SELECT clientid, SUM(CASE WHEN totalitems > completeditems THEN 1 ELSE 0 END) FROM (
+SELECT clientid, campaignid, SUM(CASE WHEN totalitems > completeditems THEN 1 ELSE 0 END) FROM (
   SELECT
-    clientid, requestid,
+    clientid, campaignid, requestid,
     COUNT(*) AS totalitems,
     SUM(CASE WHEN status IN ('FAILED', 'DELIVERED') THEN 1 ELSE 0 END) AS completeditems,
     DATE(MIN(createdtime)) AS createddate
   FROM request_item_status
-  GROUP BY clientid, requestid
+  GROUP BY clientid, campaignid, requestid
 )
 WHERE
 createddate < DATE_ADD('week', -2, CURRENT_DATE) AND
 createddate >= DATE_ADD('day', -90, CURRENT_DATE)
-GROUP BY clientid
+GROUP BY clientid, campaignid
