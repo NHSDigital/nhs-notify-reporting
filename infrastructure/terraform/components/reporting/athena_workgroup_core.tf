daab160e9bb642ce0bc5b887cf881dd2bff4694a
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-02-05 12:22:36 +0000

    CCM-8360: S3 Bucket Keys

--- infrastructure/terraform/components/reporting/athena_workgroup_core.tf
+++ infrastructure/terraform/components/reporting/athena_workgroup_core.tf
@@ -10,11 +10,6 @@ resource "aws_athena_workgroup" "core" {
       expected_bucket_owner = var.core_account_id
       output_location       = "s3://comms-${var.core_account_id}-eu-west-2-${var.core_env}-api-rpt-ingress/"
 
-      encryption_configuration {
-        encryption_option = "SSE_KMS"
-        kms_key_arn       = "arn:aws:kms:eu-west-2:${var.core_account_id}:alias/comms-${var.core_env}-api-s3"
-      }
-
       acl_configuration {
         s3_acl_option = "BUCKET_OWNER_FULL_CONTROL"
       }
