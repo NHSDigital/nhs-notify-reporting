c33b1b9150be5360e16fd40be2ade5f190adfc3f
jamesthompson26-nhs <115219893+jamesthompson26-nhs@users.noreply.github.com>
2025-02-06 09:25:08 +0000

    CCM-8360: S3 Bucket Keys (#89)
    * CCM-8360: S3 Bucket Keys
    
    * CCM-8360: S3 Bucket Keys
    
    * CCM-8360: S3 Bucket Keys
    
    * CCM-8360: S3 Bucket Keys
    
    * CCM-8360: S3 Bucket Keys

--- infrastructure/terraform/components/reporting/athena_workgroup_setup.tf
+++ infrastructure/terraform/components/reporting/athena_workgroup_setup.tf
@@ -9,11 +9,6 @@ resource "aws_athena_workgroup" "setup" {
     result_configuration {
       expected_bucket_owner = local.this_account
       output_location       = "s3://${aws_s3_bucket.results.bucket}/setup/"
-
-      encryption_configuration {
-        encryption_option = "SSE_KMS"
-        kms_key_arn       = aws_kms_key.s3.arn
-      }
     }
   }
 }
