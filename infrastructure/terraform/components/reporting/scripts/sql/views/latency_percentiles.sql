96be1abf1c9a0c902176361fa9a3407c567df268
markpullan <mark.pullan@bjss.com>
2025-03-03 16:36:05 +0000

    Feature/ccm 6801 athena views fix (#98)
    * Fix query bug
    
    * Use local TZ for unsociable hours

--- infrastructure/terraform/components/reporting/scripts/sql/views/latency_percentiles.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/views/latency_percentiles.sql
@@ -46,8 +46,8 @@ CROSS JOIN UNNEST (ARRAY[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99
 WHERE starttime IS NOT NULL
 AND endtime IS NOT NULL
 AND starttime > DATE('2000-01-01')
-AND DAY_OF_month(starttime) <= 5
-AND HOUR(starttime) < 16
-AND HOUR(starttime) >= 9
+AND DAY_OF_WEEK(starttime) <= 5
+AND HOUR(AT_TIMEZONE(starttime, 'Europe/London')) < 18
+AND HOUR(AT_TIMEZONE(starttime, 'Europe/London')) >= 8
 GROUP BY clientid, campaignid, communicationtype, percentile
 ORDER BY clientid, campaignid, communicationtype, percentile
