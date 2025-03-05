249c5d4dacbdcad965c3d4fe0df2282fab538b38
markpullan <mark.pullan@bjss.com>
2025-03-05 14:28:04 +0000

    CCM-6801 athena view iteration (#99)
    * Query tweaks following dashboard review
    
    * Additional bucket

--- infrastructure/terraform/components/reporting/scripts/sql/views/latency_percentiles.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/views/latency_percentiles.sql
@@ -10,11 +10,12 @@ FROM (
   SELECT rip.clientid, rip.campaignid, rip.communicationtype, rq.rqcreatedtime AS starttime, rip.sendtime AS endtime FROM
     (
       SELECT requestid, MIN(createdtime) AS rqcreatedtime FROM request_item_status
+      --Query optimisation to prevent full table scan on request_item_status createdtime
+      WHERE createdtime >= DATE_ADD('week', -1, DATE_ADD('month', -2, CURRENT_DATE))
       GROUP BY requestid
     ) AS rq
     INNER JOIN request_item_plan_status rip ON rq.requestid = rip.requestid
     WHERE rip.createdtime >= DATE_ADD('month', -2, CURRENT_DATE)
-    AND rq.rqcreatedtime >= DATE_ADD('month', -2, CURRENT_DATE)
     AND rip.ordernumber = 1
     AND rip.channeltype = 'primary'
   UNION ALL
@@ -42,7 +43,7 @@ FROM (
   )
   WHERE ordernumber > 1
 )
-CROSS JOIN UNNEST (ARRAY[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99, 0.999]) AS t(percentile)
+CROSS JOIN UNNEST (ARRAY[0.001, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.96, 0.97, 0.98, 0.99, 0.999]) AS t(percentile)
 WHERE starttime IS NOT NULL
 AND endtime IS NOT NULL
 AND starttime > DATE('2000-01-01')
