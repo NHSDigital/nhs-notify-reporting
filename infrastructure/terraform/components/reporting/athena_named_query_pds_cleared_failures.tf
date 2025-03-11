0428820cc01f9ac0f35f90702ef3c86b846f3a75
Mark Pullan <mark.pullan@bjss.com>
2025-03-11 13:25:44 +0000

    Rename query

--- infrastructure/terraform/components/reporting/athena_named_query_pds_cleared_failures.tf
+++ infrastructure/terraform/components/reporting/athena_named_query_pds_cleared_failures.tf
@@ -1,9 +1,9 @@
-resource "aws_athena_named_query" "pds_contact_corrections" {
-  name        = "pds_contact_corrections"
+resource "aws_athena_named_query" "pds_cleared_failures" {
+  name        = "pds_cleared_failures"
   description = "Query to report which pds changes may have resulted in the correction of a temporary or permanent failure"
   workgroup   = aws_athena_workgroup.user.id
   database    = aws_glue_catalog_database.reporting.name
-  query       = file("${path.module}/scripts/sql/reports/pds_contact_corrections.sql")
+  query       = file("${path.module}/scripts/sql/reports/pds_cleared_failures.sql")
 
   depends_on = [
     null_resource.request_item_status_table,
