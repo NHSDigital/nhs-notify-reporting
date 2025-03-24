65996422cf62c127562770203b6af5bc27abf905
markpullan <mark.pullan@bjss.com>
2025-03-24 10:56:27 +0000

    Add watchdog for stuck items (#102)

---
+++ infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_stuck_request_items.tf
@@ -0,0 +1,14 @@
+resource "aws_cloudwatch_metric_alarm" "stuck_request_items" {
+  alarm_name          = "${local.csi}-stuck-request-items"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  threshold           = 1
+  alarm_description   = "Request items stuck in an ENRICHED or PENDING_ENRICHMENT state for longer than an expected time window"
+
+  metric_query {
+    id          = "max_stuck_request_items_count"
+    expression  = "SELECT MAX(StuckRequestItemsCount) FROM \"Notify/Watchdog\" WHERE environment='${var.environment}'"
+    return_data = "true"
+    period      = 3600
+  }
+}
