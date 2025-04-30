a4d3bf826a1c4595d395768fd527dc5c28e4d522
mark.slowey1 <mark.slowey1@nhs.net>
2025-04-30 12:44:19 +0100

    CCM-7786: formatting

--- infrastructure/terraform/components/reporting/scripts/sql/ingestion/client_latest_name.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/ingestion/client_latest_name.sql
@@ -2,15 +2,15 @@ MERGE INTO client_latest_name as target
 USING (
   SELECT clientid, clientname, CAST("$classification".timestamp AS BIGINT) AS timestamp
   FROM ${source_table}
-  WHERE (sk LIKE 'REQUEST#%' OR sk LIKE 'REQUEST_ITEM#%' OR sk LIKE 'REQUEST_ITEM_PLAN#%') 
+  WHERE (sk LIKE 'REQUEST#%' OR sk LIKE 'REQUEST_ITEM#%' OR sk LIKE 'REQUEST_ITEM_PLAN#%')
     AND (
       -- Moving 1-week ingestion window
       DATE(CAST(__year AS VARCHAR) || '-' || CAST(__month AS VARCHAR) || '-' || CAST(__day  AS VARCHAR)) >= DATE_ADD('week', -1, CURRENT_DATE)
     )
 ) as source
 ON
-  source.clientid = target.clientid 
-WHEN MATCHED AND (source.timestamp > target.timestamp) THEN UPDATE SET 
+  source.clientid = target.clientid
+WHEN MATCHED AND (source.timestamp > target.timestamp) THEN UPDATE SET
   target.clientname = source.clientname
   target.timestamp = source.timestamp
 WHEN NOT MATCHED THEN INSERT (
