4565f9bcda4beb742e78f63ffb43c63b1e3412c6
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-05-21 11:53:47 +0100

    CCM-10203: Add Observability Datasource

--- infrastructure/terraform/components/reporting/locals_remote_state.tf
+++ infrastructure/terraform/components/reporting/locals_remote_state.tf
@@ -28,11 +28,11 @@ data "terraform_remote_state" "acct" {
     bucket = local.terraform_state_bucket
 
     key = format(
-      "%s/%s/%s/%s/acct.tfstate",
+      "%s/%s/%s/acct.tfstate",
       var.project,
       var.aws_account_id,
       "eu-west-2",
-      var.parent_acct_environment
+      var.environment
     )
 
     region = "eu-west-2"
