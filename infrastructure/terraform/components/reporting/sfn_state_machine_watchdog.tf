3caa1909e114cde3dbad230f1c862f853d8ee049
Mark Pullan <mark.pullan@bjss.com>
2025-06-04 12:12:40 +0100

    IAM fix

--- infrastructure/terraform/components/reporting/sfn_state_machine_watchdog.tf
+++ infrastructure/terraform/components/reporting/sfn_state_machine_watchdog.tf
@@ -108,6 +108,7 @@ data "aws_iam_policy_document" "sfn_watchdog" {
       "arn:aws:glue:${var.region}:${local.this_account}:table/${aws_glue_catalog_database.reporting.name}/request_item_status",
       "arn:aws:glue:${var.region}:${local.this_account}:table/${aws_glue_catalog_database.reporting.name}/request_item_plan_status",
       "arn:aws:glue:${var.region}:${local.this_account}:table/${aws_glue_catalog_database.reporting.name}/request_item_status_summary",
+      "arn:aws:glue:${var.region}:${local.this_account}:table/${aws_glue_catalog_database.reporting.name}/raw_latency_3m",
     ]
   }
 
