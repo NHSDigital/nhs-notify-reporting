bc96d0200ef416e0c807a17be203bb314281acb1
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-26 12:13:29 +0100

    CCM-10036: Cost Anomaly Alarms and Alerts

--- infrastructure/terraform/components/acct/sns_topic_costs.tf
+++ infrastructure/terraform/components/acct/sns_topic_costs.tf
@@ -1,28 +1,37 @@
-resource "aws_ce_anomaly_monitor" "anomaly_monitor" {
-  name              = "${local.csi}-anomaly-monitor"
-  monitor_type      = "DIMENSIONAL"
-  monitor_dimension = "SERVICE"
+resource "aws_sns_topic" "costs" {
+  name              = "${local.csi}-costs"
+  kms_master_key_id = module.kms.key_id
 }
 
-resource "aws_ce_anomaly_subscription" "realtime_subscription" {
-  name      = "${local.csi}-realtime-subscription"
-  frequency = "IMMEDIATE"
-  threshold_expression {
-    dimension {
-      key           = "ANOMALY_TOTAL_IMPACT_PERCENTAGE"
-      values        = [var.cost_anomaly_threshold]
-      match_options = ["GREATER_THAN_OR_EQUAL"]
+resource "aws_sns_topic_policy" "costs" {
+  arn = aws_sns_topic.costs.arn
+
+  policy = data.aws_iam_policy_document.sns_costs.json
+}
+
+data "aws_iam_policy_document" "sns_costs" {
+  statement {
+    sid    = "AllowSNSCosts"
+    effect = "Allow"
+
+    actions = [
+      "SNS:Publish",
+    ]
+
+    resources = [
+      aws_sns_topic.costs.arn,
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["budgets.amazonaws.com", "costalerts.amazonaws.com"]
     }
   }
-  monitor_arn_list = [
-    aws_ce_anomaly_monitor.anomaly_monitor.arn,
-  ]
+}
 
-  subscriber {
-    type    = "SNS"
-    address = aws_sns_topic.costs.arn
-  }
-  depends_on = [
-    aws_sns_topic_policy.costs,
-  ]
+resource "aws_sns_topic_subscription" "costs" {
+  for_each  = toset(var.cost_alarm_recipients)
+  topic_arn = aws_sns_topic.costs.arn
+  protocol  = "email"
+  endpoint  = each.value
 }
