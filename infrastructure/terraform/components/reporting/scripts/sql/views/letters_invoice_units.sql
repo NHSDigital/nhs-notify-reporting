057d474bd4953703316b4631dc21604f9350ce26
Mark Pullan <mark.pullan@bjss.com>
2025-07-09 11:44:40 +0100

    Fix month-end logic bug

--- infrastructure/terraform/components/reporting/scripts/sql/views/letters_invoice_units.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/views/letters_invoice_units.sql
@@ -5,18 +5,18 @@ FROM (
         CASE
             WHEN supplier='MBA' THEN
                 CASE
-                    WHEN DAY_OF_WEEK(sendtime)=4 AND MONTH(sendtime)!=MONTH(DATE_ADD('day', 4, sendtime)) THEN DATE_ADD('day', 4, sendtime)
-                    WHEN DAY_OF_WEEK(sendtime)=5 AND MONTH(sendtime)!=MONTH(DATE_ADD('day', 3, sendtime)) THEN DATE_ADD('day', 3, sendtime)
-                    WHEN DAY_OF_WEEK(sendtime)=6 AND MONTH(sendtime)!=MONTH(DATE_ADD('day', 2, sendtime)) THEN DATE_ADD('day', 2, sendtime)
-                    WHEN DAY_OF_WEEK(sendtime)=7 AND MONTH(sendtime)!=MONTH(DATE_ADD('day', 1, sendtime)) THEN DATE_ADD('day', 1, sendtime)
-                    ELSE sendtime
+                    WHEN DAY_OF_WEEK(sendtime)=4 THEN DATE_ADD('day', 4, sendtime)
+                    WHEN DAY_OF_WEEK(sendtime)=5 THEN DATE_ADD('day', 4, sendtime)
+                    WHEN DAY_OF_WEEK(sendtime)=6 THEN DATE_ADD('day', 4, sendtime)
+                    WHEN DAY_OF_WEEK(sendtime)=7 THEN DATE_ADD('day', 3, sendtime)
+                    ELSE DATE_ADD('day', 2, sendtime)
                 END
             ELSE sendtime
         END AS invoicetime
     FROM request_item_plan_status
     WHERE communicationtype='LETTER'
     AND sendtime IS NOT NULL
-    AND status = 'DELIVERED'
+    AND status='DELIVERED'
 )
 GROUP BY clientid, campaignid, supplier, YEAR(invoicetime), MONTH(invoicetime)
 ORDER BY clientid, campaignid, supplier, YEAR(invoicetime), MONTH(invoicetime)
