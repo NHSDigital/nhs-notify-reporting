637273477ac4c8c79a09d171f6dd515024831a89
Mark Pullan <mark.pullan@bjss.com>
2025-09-04 12:35:48 +0100

    Add new view to IAM

--- infrastructure/terraform/components/reporting/null_resource_request_item_status_summary_all_view.tf
+++ infrastructure/terraform/components/reporting/null_resource_request_item_status_summary_all_view.tf
@@ -1,6 +1,5 @@
 resource "null_resource" "request_item_status_summary_all_view" {
   triggers = {
-    always_run = timestamp()
     sql = filesha256("${path.module}/scripts/sql/views/request_item_status_summary_all.sql")
   }
   provisioner "local-exec" {
