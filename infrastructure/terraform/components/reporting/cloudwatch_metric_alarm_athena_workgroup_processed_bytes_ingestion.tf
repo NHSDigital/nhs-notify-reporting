fa55373f6f1def7653e00a0039a84f6e3b063cc6
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-09-29 14:01:45 +0100

    CCM-12280: Cost Spike Indicator Athena Workgroup

--- infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_athena_workgroup_processed_bytes_ingestion.tf
+++ infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_athena_workgroup_processed_bytes_ingestion.tf
@@ -1,15 +1,29 @@
 resource "aws_cloudwatch_metric_alarm" "athena_workgroup_processed_bytes_ingestion" {
   alarm_name          = "${local.csi}-athena-workgroup-processed-bytes-ingestion"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
+  comparison_operator = "GreaterThanUpperThreshold"
   evaluation_periods  = 1
-  metric_name         = "ProcessedBytes"
-  namespace           = "AWS/Athena"
-  period              = 300
-  statistic           = "Sum"
-  threshold           = 1000000000
-  alarm_description   = "Alarm for spikes in Athena Workgroup ProcessedBytes (ingestion)."
+  alarm_description   = "Alarm for anomalous spikes in Athena Workgroup ProcessedBytes (ingestion)."
   treat_missing_data  = "notBreaching"
-  dimensions = {
-    WorkGroup = aws_athena_workgroup.ingestion.name
+  threshold_metric_id = "ad1"
+
+  metric_query {
+    id          = "m1"
+    metric {
+      metric_name = "ProcessedBytes"
+      namespace   = "AWS/Athena"
+      period      = 300
+      stat        = "Sum"
+      dimensions = {
+        WorkGroup = aws_athena_workgroup.ingestion.name
+      }
+    }
+    return_data = false
+  }
+
+  metric_query {
+    id          = "ad1"
+    expression  = "ANOMALY_DETECTION_BAND(m1, 2)"
+    label       = "Anomaly Detection Band"
+    return_data = true
   }
 }
--- infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_athena_workgroup_processed_bytes_setup.tf
+++ infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_athena_workgroup_processed_bytes_setup.tf
@@ -1,15 +1,29 @@
 resource "aws_cloudwatch_metric_alarm" "athena_workgroup_processed_bytes_setup" {
   alarm_name          = "${local.csi}-athena-workgroup-processed-bytes-setup"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
+  comparison_operator = "GreaterThanUpperThreshold"
   evaluation_periods  = 1
-  metric_name         = "ProcessedBytes"
-  namespace           = "AWS/Athena"
-  period              = 300
-  statistic           = "Sum"
-  threshold           = 1000000000
-  alarm_description   = "Alarm for spikes in Athena Workgroup ProcessedBytes (setup)."
+  alarm_description   = "Alarm for anomalous spikes in Athena Workgroup ProcessedBytes (setup)."
   treat_missing_data  = "notBreaching"
-  dimensions = {
-    WorkGroup = aws_athena_workgroup.setup.name
+  threshold_metric_id = "ad1"
+
+  metric_query {
+    id          = "m1"
+    metric {
+      metric_name = "ProcessedBytes"
+      namespace   = "AWS/Athena"
+      period      = 300
+      stat        = "Sum"
+      dimensions = {
+        WorkGroup = aws_athena_workgroup.setup.name
+      }
+    }
+    return_data = false
+  }
+
+  metric_query {
+    id          = "ad1"
+    expression  = "ANOMALY_DETECTION_BAND(m1, 2)"
+    label       = "Anomaly Detection Band"
+    return_data = true
   }
 }
