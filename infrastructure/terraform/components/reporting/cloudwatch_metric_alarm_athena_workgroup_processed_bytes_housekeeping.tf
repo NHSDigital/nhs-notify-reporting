5ed70afb19f826e5a7ac3bb7b6b49582ab245538
jamesthompson26-nhs <115219893+jamesthompson26-nhs@users.noreply.github.com>
2025-09-30 14:15:45 +0100

    Feature/CCM-12280 cost spike indicator athena workgroup (#155)
    * CCM-12280: Athena_Workgroup_Processed_Bytes_Alarms
    
    * CCM-12280: Athena_Workgroup_Processed_Bytes_Alarms
    
    * CCM-12280: Athena_Workgroup_Processed_Bytes_Alarms
    
    * CCM-12280: Cost Spike Indicator Athena Workgroup
    
    * CCM-12280: Cost Spike Indicator Athena Workgroup

---
+++ infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_athena_workgroup_processed_bytes_housekeeping.tf
@@ -0,0 +1,29 @@
+resource "aws_cloudwatch_metric_alarm" "athena_workgroup_processed_bytes_housekeeping" {
+  alarm_name          = "${local.csi}-athena-workgroup-processed-bytes-housekeeping"
+  comparison_operator = "GreaterThanUpperThreshold"
+  evaluation_periods  = 1
+  alarm_description   = "Alarm for anomalous spikes in Athena Workgroup ProcessedBytes (housekeeping)."
+  treat_missing_data  = "notBreaching"
+  threshold_metric_id = "ad1"
+
+  metric_query {
+    id          = "m1"
+    metric {
+      metric_name = "ProcessedBytes"
+      namespace   = "AWS/Athena"
+      period      = 300
+      stat        = "Sum"
+      dimensions = {
+        WorkGroup = aws_athena_workgroup.housekeeping.name
+      }
+    }
+    return_data = true
+  }
+
+  metric_query {
+    id          = "ad1"
+    expression  = "ANOMALY_DETECTION_BAND(m1, 2)"
+    label       = "Anomaly Detection Band"
+    return_data = true
+  }
+}
