b89cd85cd1e36a7b5fa8be37b1369c8da5e52f2f
Mark Pullan <mark.pullan@bjss.com>
2025-10-07 16:38:23 +0100

    Reduce alarm sensitivity

--- infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_degraded_latency.tf
+++ infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_degraded_latency.tf
@@ -1,15 +1,25 @@
+locals {
+  alarm_period = 3600
+}
+
 resource "aws_cloudwatch_metric_alarm" "degraded_latency" {
   alarm_name          = "${local.csi}-degraded-latency"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
-  threshold           = 1
-  alarm_description   = "Today's latencies are significantly higher than historic trends"
+  threshold           = 2
+  alarm_description   = "Triggers when multiple degraded-latency metrics are nonzero"
   treat_missing_data  = "notBreaching"
 
   metric_query {
-    id          = "max_degraded_latency_count"
-    expression  = "SELECT MAX(DegradedLatenciesCount) FROM \"Notify/Watchdog\" WHERE environment='${var.environment}'"
-    return_data = "true"
-    period      = 3600
+    id          = "count_degraded"
+    expression  = <<-EOT
+      SELECT COUNT_IF(DegradedLatenciesCount > 0)
+      FROM "Notify/Watchdog"
+      WHERE environment='${var.environment}'
+      GROUP BY PERIOD(${local.alarm_period} SECONDS)
+      FILL(0)
+    EOT
+    return_data = true
+    period      = local.alarm_period
   }
 }
