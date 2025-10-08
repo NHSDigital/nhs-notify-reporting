51dd523aef6dda9e108e940136b4ee3d802e288d
Mark Pullan <mark.pullan@bjss.com>
2025-10-08 09:06:07 +0100

    Fix SQL

--- infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_degraded_latency.tf
+++ infrastructure/terraform/components/reporting/cloudwatch_metric_alarm_degraded_latency.tf
@@ -1,25 +1,26 @@
-locals {
-  alarm_period = 3600
-}
-
 resource "aws_cloudwatch_metric_alarm" "degraded_latency" {
   alarm_name          = "${local.csi}-degraded-latency"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   threshold           = 2
-  alarm_description   = "Triggers when multiple degraded-latency metrics are nonzero"
+  alarm_description   = "Triggers when multiple clients/campaigns experience degraded latencies simultaneously"
   treat_missing_data  = "notBreaching"
 
   metric_query {
-    id          = "count_degraded"
-    expression  = <<-EOT
-      SELECT COUNT_IF(DegradedLatenciesCount > 0)
+    id     = "degraded_latencies_count_max"
+    expression = <<-SQL
+      SELECT MAX(DegradedLatenciesCount)
       FROM "Notify/Watchdog"
       WHERE environment='${var.environment}'
-      GROUP BY PERIOD(${local.alarm_period} SECONDS)
-      FILL(0)
-    EOT
+      GROUP BY environment, clientid, campaignid
+    SQL
+    return_data = false
+    period = 3600
+  }
+
+  metric_query {
+    id          = "degraded_client_campaign_count"
+    expression  = "SUM(IF(degraded_latencies_count_max > 0, 1, 0))"
     return_data = true
-    period      = local.alarm_period
   }
 }
