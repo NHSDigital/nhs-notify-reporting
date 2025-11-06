7334a675c617a28c5d4589821c2d4d0c8c5340fc
sidnhs <siddharth.nair1@nhs.net>
2025-11-06 12:47:42 +0000

    CCM-12323: Fix backup selection plan name (#171)
    * CCM-12323: Fix backup selection plan name
    
    * CCM-12323: Fix backup selection plan name

--- infrastructure/terraform/components/reporting/backup_restore_testing_selection_comms_backup_restore_selection_s3.tf
+++ infrastructure/terraform/components/reporting/backup_restore_testing_selection_comms_backup_restore_selection_s3.tf
@@ -1,7 +1,7 @@
 resource "aws_backup_restore_testing_selection" "backup_restore_testing_selection_s3" {
   count = var.enable_s3_backup ? 1 : 0
   name                      = replace("${local.csi}_s3_backup_restore", "-", "_")
-  restore_testing_plan_name = "${local.csi}_restore_testing_plan"
+  restore_testing_plan_name = aws_backup_plan.s3_backup[0].name
   protected_resource_type   = "S3"
   iam_role_arn              = aws_iam_role.s3_backup[0].arn
   protected_resource_conditions {
