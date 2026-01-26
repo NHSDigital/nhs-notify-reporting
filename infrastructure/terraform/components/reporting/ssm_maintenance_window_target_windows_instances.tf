af97de813d999eeb087fcb11e9d9f96f4aaa4a0d
jamesthompson26-nhs <james.thompson26@nhs.net>
2026-01-26 09:51:39 +0000

    CCM-13905: Gateway Improvements

--- infrastructure/terraform/components/reporting/ssm_maintenance_window_target_windows_instances.tf
+++ infrastructure/terraform/components/reporting/ssm_maintenance_window_target_windows_instances.tf
@@ -1,24 +1,10 @@
-resource "aws_ssm_maintenance_window_target" "windows_instances_sunday_asg" {
-  count = var.enable_powerbi_gateway ? 1 : 0
-
-  description   = "Windows Server 2022 Sunday Maintenance Window Target (ASG)"
-  window_id     = aws_ssm_maintenance_window.patch_window_sunday[0].id
-  resource_type = "INSTANCE"
-  name          = "${local.csi}-maintenance-window-target-sun-asg"
-
-  targets {
-    key    = "tag:Patch Group"
-    values = ["${local.csi}-windows-group"]
-  }
-}
-
-resource "aws_ssm_maintenance_window_target" "windows_instances_sunday_standalone" {
+resource "aws_ssm_maintenance_window_target" "windows_instances_sunday" {
   count = var.enable_powerbi_gateway && var.powerbi_gateway_instance_count >= 1 ? 1 : 0
 
-  description   = "Windows Server 2022 Sunday Maintenance Window Target (Standalone)"
+  description   = "Windows Server 2022 Sunday Maintenance Window Target "
   window_id     = aws_ssm_maintenance_window.patch_window_sunday[0].id
   resource_type = "INSTANCE"
-  name          = "${local.csi}-maintenance-window-target-sun-standalone"
+  name          = "${local.csi}-maintenance-window-target-sun"
 
   targets {
     key    = "InstanceIds"
@@ -37,5 +23,20 @@ resource "aws_ssm_maintenance_window_target" "windows_instances_wednesday" {
   targets {
     key    = "InstanceIds"
     values = [aws_instance.powerbi_gateway_standalone[1].id]
+  }
+}
+
+## Remove me later - replaced by above two targets
+resource "aws_ssm_maintenance_window_target" "windows_instances" {
+  count = var.enable_powerbi_gateway ? 1 : 0
+
+  description   = "Windows Server 2022 Maintenance Window Target"
+  window_id     = aws_ssm_maintenance_window.patch_window[0].id
+  resource_type = "INSTANCE"
+  name          = "${local.csi}-maintenance-window-target"
+
+  targets {
+    key    = "tag:Patch Group"
+    values = ["${local.csi}-windows-group"]
   }
 }
--- infrastructure/terraform/components/reporting/ssm_maintenance_window_task_patch_task.tf
+++ infrastructure/terraform/components/reporting/ssm_maintenance_window_task_patch_task.tf
@@ -8,10 +8,7 @@ resource "aws_ssm_maintenance_window_task" "patch_task_sunday" {
 
   targets {
     key = "WindowTargetIds"
-    values = concat(
-      [aws_ssm_maintenance_window_target.windows_instances_sunday_asg[0].id],
-      var.powerbi_gateway_instance_count >= 1 ? [aws_ssm_maintenance_window_target.windows_instances_sunday_standalone[0].id] : []
-    )
+    values = [aws_ssm_maintenance_window_target.windows_instances_sunday_standalone[0].id]
   }
 
   task_invocation_parameters {
@@ -62,5 +59,34 @@ resource "aws_ssm_maintenance_window_task" "patch_task_wednesday" {
 
   priority        = 1
   max_concurrency = "1"
+  max_errors      = "1"
+}
+
+## Remove me later - replaced by above two tasks
+resource "aws_ssm_maintenance_window_task" "patch_task" {
+  count = var.enable_powerbi_gateway ? 1 : 0
+
+  description = "Windows Server 2022 Patch Task"
+  window_id   = aws_ssm_maintenance_window.patch_window[0].id
+  task_arn    = "AWS-RunPatchBaseline"
+  task_type   = "RUN_COMMAND"
+
+  targets {
+    key    = "WindowTargetIds"
+    values = [aws_ssm_maintenance_window_target.windows_instances[0].id]
+  }
+
+  task_invocation_parameters {
+    run_command_parameters {
+      comment = "Patching Windows Instances"
+      parameter {
+        name   = "Operation"
+        values = ["Install"]
+      }
+    }
+  }
+
+  priority        = 1
+  max_concurrency = "2"
   max_errors      = "1"
 }
