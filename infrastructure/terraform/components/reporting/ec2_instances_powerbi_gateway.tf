f979d36b7eb81fd15d68d76c17553037b3ec9e6e
jamesthompson26-nhs <james.thompson26@nhs.net>
2026-01-26 10:40:23 +0000

    CCM-13905: Gateway Improvements

--- infrastructure/terraform/components/reporting/ec2_instances_powerbi_gateway.tf
+++ infrastructure/terraform/components/reporting/ec2_instances_powerbi_gateway.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "powerbi_gateway_standalone" {
   count = var.enable_powerbi_gateway ? var.powerbi_gateway_instance_count : 0
 
+  associate_public_ip_address = false
   launch_template {
     id      = aws_launch_template.powerbi_gateway_standalone[0].id
     version = "$Latest"
