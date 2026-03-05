a37ffc3fca59810b6b4923043f134ca45730f114
Mark Pullan <mark.pullan@bjss.com>
2026-03-05 14:47:59 +0000

    Bill for text messages that are sent to GUN but then fail delivery

--- infrastructure/terraform/components/reporting/scripts/sql/views/billing_transactions.sql
+++ infrastructure/terraform/components/reporting/scripts/sql/views/billing_transactions.sql
@@ -17,8 +17,15 @@ ON
     rip.clientid = ri.clientid AND
     rip.requestitemid = ri.requestitemid
 WHERE
-    rip.status='DELIVERED' AND
-    rip.communicationtype IN ('SMS', 'LETTER') AND
-    rip.completedtime IS NOT NULL
+    rip.completedtime IS NOT NULL AND
+    (
+        (
+            --Bill for all text messages forwarded to the supplier
+            (rip.communicationtype ='SMS') AND (rip.status IN ('DELIVERED', 'FAILED')) AND (sendtime IS NOT NULL)
+        ) OR (
+            --Bill for all letters accepted by the supplier
+            (rip.communicationtype ='LETTER') AND (rip.status='DELIVERED')
+        )
+    )
 GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 ORDER BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
