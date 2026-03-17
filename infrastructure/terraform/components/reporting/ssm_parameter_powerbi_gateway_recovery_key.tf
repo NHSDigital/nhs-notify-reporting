9469a5a10e20b5c3275ba055e65ba98e7d11e9d2
jamesthompson26-nhs <james.thompson26@nhs.net>
2026-03-17 16:05:41 +0000

    CCM-14316: Gateway Cluster Deployment

--- infrastructure/terraform/components/reporting/ssm_parameter_powerbi_gateway_recovery_key.tf
+++ infrastructure/terraform/components/reporting/ssm_parameter_powerbi_gateway_recovery_key.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "powerbi_gateway_recovery_key" {
   count = var.enable_powerbi_gateway ? 1 : 0
 
   name        = "/${local.csi}/powerbi-gateway-recovery-key"
-  description = "The Recovery Key for the On-Premises Gateway"
+  description = "The Recovery Key for the On-Premises Gateway - Updated manually with the actual key value after deployment"
   type        = "SecureString"
   value       = "RECOVERY_KEY_PLACEHOLDER"
 
