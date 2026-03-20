6b644a47bbaba9632eab202d43256056b2edb3c8
Ian Hodges <ian.hodges1@nhs.net>
2026-03-20 14:53:16 +0000

    CCM-15446: add request_item_plan_read_status_smsnudge view

---
+++ infrastructure/terraform/components/reporting/scripts/sql/views/request_item_plan_read_status_smsnudge.sql
@@ -0,0 +1,23 @@
+SELECT
+  date(nudge.createdtime) createddate,
+  nudge.clientid,
+  nudge.requestitemplanid,
+  originatingclientid,
+  originatingcampaignid,
+  originatingbillingrefid,
+  originalsendinggroupid,
+  originatingrequestitemplanid,
+  app.status as originalStatus,
+  case
+    when nudge.status = 'DELIVERED'
+    and app.status = 'DELIVERED' then date_diff('second', nudge.createdtime, app.completedtime) / 3600.0 else null
+  end hrsToRead
+FROM
+  "nhs-notify-prod-reporting-database"."request_item_plan_status_smsnudge" nudge
+left join
+  "nhs-notify-prod-reporting-database"."request_item_plan_status" app
+on (
+  nudge.originatingrequestitemplanid = app.requestitemplanid
+  and nudge.originatingclientid = app.clientid
+  and app.communicationtype = 'NHSAPP'
+)
